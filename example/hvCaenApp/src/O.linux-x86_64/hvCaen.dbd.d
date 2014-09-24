0f45437fc07e8dc64db5e5b9ea9c1e3f3bc790c2
Per Hansson Adrian <phansson@slac.stanford.edu>
2014-09-24 12:24:41 -0700

    remove build and temp files

--- example/hvCaenApp/src/O.linux-x86_64/hvCaen.dbd.d
+++
@@ -1,7 +0,0 @@
-# DO NOT EDIT: This file created by mkmf.pl,v 1.5 2002/03/25 21:33:24 jba Exp $ 
-
-../O.Common/hvCaen.dbd : /home/tracker/pelle/epics/drivers/dbd/HVCAENx527.dbd
-../O.Common/hvCaen.dbd : /home/tracker/pelle/epics/drivers/dbd/LVMPOD.dbd
-
-
-../hvCaenInclude.dbd : ../Makefile
