c760d8ecf6fa350bb734605e25b5c1af5d38c9fc
Per Hansson Adrian <phansson@slac.stanford.edu>
2014-09-24 18:32:40 -0700

    adding mpod driver dir

---
+++ drivers/HVCAENx527App/src/O.linux-x86_64/HVCAENx527chBio.d
@@ -0,0 +1,33 @@
+HVCAENx527chBio.o: ../HVCAENx527chBio.c ../HVCAENx527.h \
+  /u1/packages/base-3.14.12.2//include/dbDefs.h \
+  /u1/packages/base-3.14.12.2//include/dbAccess.h \
+  /u1/packages/base-3.14.12.2//include/dbDefs.h \
+  /u1/packages/base-3.14.12.2//include/epicsTime.h \
+  /u1/packages/base-3.14.12.2//include/shareLib.h \
+  /u1/packages/base-3.14.12.2//include/epicsTypes.h \
+  /u1/packages/base-3.14.12.2//include/os/Linux/osdTime.h \
+  /u1/packages/base-3.14.12.2//include/caeventmask.h \
+  /u1/packages/base-3.14.12.2//include/dbFldTypes.h \
+  /u1/packages/base-3.14.12.2//include/link.h \
+  /u1/packages/base-3.14.12.2//include/dbBase.h \
+  /u1/packages/base-3.14.12.2//include/ellLib.h \
+  /u1/packages/base-3.14.12.2//include/dbAddr.h \
+  /u1/packages/base-3.14.12.2//include/dbLock.h \
+  /u1/packages/base-3.14.12.2//include/dbAccessDefs.h \
+  /u1/packages/base-3.14.12.2//include/errMdef.h \
+  /u1/packages/base-3.14.12.2//include/dbCa.h \
+  /u1/packages/base-3.14.12.2//include/dbCommon.h \
+  /u1/packages/base-3.14.12.2//include/epicsMutex.h \
+  /u1/packages/base-3.14.12.2//include/epicsAssert.h \
+  /u1/packages/base-3.14.12.2//include/os/Linux/osdMutex.h \
+  /u1/packages/base-3.14.12.2//include/db_field_log.h \
+  /u1/packages/base-3.14.12.2//include/recSup.h \
+  /u1/packages/base-3.14.12.2//include/devSup.h \
+  /u1/packages/base-3.14.12.2//include/callback.h \
+  /u1/packages/base-3.14.12.2//include/errlog.h \
+  /u1/packages/base-3.14.12.2//include/compilerDependencies.h \
+  ../CAENHVWrapper.h ../caenhvoslib.h \
+  /u1/packages/base-3.14.12.2//include/biRecord.h \
+  /u1/packages/base-3.14.12.2//include/boRecord.h \
+  /u1/packages/base-3.14.12.2//include/epicsExport.h \
+  /u1/packages/base-3.14.12.2//include/shareLib.h
