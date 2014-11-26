458db1b2c945580291cad97a4d458ff8ed1784a1
perhansson <per.hansson80@gmail.com>
2014-11-26 12:45:52 -0800

    rem mpod stuff from svt daq dir

--- svt-daq-epics/drivers/LVMPODApp/src/O.linux-x86_64/LVMPOD.d
+++
@@ -1,6 +0,0 @@
-LVMPOD.o: ../LVMPOD.cpp \
-  /u1/packages/base-3.14.12.2//include/epicsExport.h \
-  /u1/packages/base-3.14.12.2//include/shareLib.h \
-  /u1/packages/base-3.14.12.2//include/iocsh.h \
-  /u1/packages/base-3.14.12.2//include/shareLib.h ../LVMPODCrate.h \
-  ../LVMPOD.h
